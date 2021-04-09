625eca08dc3903a41af97895397b149fc68f39de
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-04-09 17:27:27 +0900

    画像の追加

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,26 @@ export class App extends React.Component {
     })
   }
 
+  initChats = () => {
+    const initDataset = this.state.dataset[this.state.currentId];
+    const chat = {
+      text: initDataset.question, 
+      type: 'question'
+    }
+
+    const chats = this.state.chats;
+    chats.push(chat)
+
+
+
+
+    this.setState({
+      chats: chats
+    })
+  }
+
 componentDidMount(){
+  this.initChats()
   this.initAnswer()
 }
 
@@ -32,7 +51,7 @@ componentDidMount(){
     return (
       <section className="c-section">
         <div className="c-box">
-          <Chats/>
+          <Chats chats={this.state.chats}/>
           <AnswersList answers={this.state.answers} />
         </div>
       </section>
--- src/components/chats.jsx
+++ src/components/chats.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
+import {Chat} from "./index"
 
-const Chats = () => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: '36ch',
+    backgroundColor: theme.palette.background.paper,
+  }, 
+}));
+
+const Chats = (props) => {
+  const classes = useStyles();
+  return(
+    <List className={classes.root}> 
+      {props.chats.map((chat, index) => {
+        return <Chat text={chat.text} type={chat.type} key={index.toString()}/>
+      })}
+    </ List>
+  )
 
 }
 
