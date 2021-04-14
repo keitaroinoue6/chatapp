d69a25ec0773b018279d9b20c3b0d5ea82e820d6
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2021-04-14 14:34:24 +0900

    firebaseへの連携

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import defaultDataset from "./dataset"; //defauleDatesetを読み込む
 import './assets/styles/style.css'
 import { AnswersList, Chats } from "./components/index"; 
 import FormDialog from './components/Forms/FormDialog';
+import {db} from './firebase/index'
 
 export class App extends React.Component {
   constructor(props) { //初期のstateの状態の定義
@@ -11,7 +11,7 @@ export class App extends React.Component {
       answers: [],
       chats: [],
       currentId: "init",
-      dataset: defaultDataset,
+      dataset: {},
       open: false
     }
     this.selectAnswer = this.selectAnswer.bind(this)
@@ -79,12 +79,28 @@ export class App extends React.Component {
     this.setState({open: false});
   };
 
+  initDataset = (dataset) => {
+    this.setState({dataset: dataset})
+  }
 
 
 
   componentDidMount(){
-    const initAnswer = "";
-    this.selectAnswer(initAnswer, this.state.currentId)
+    (async() => {
+      const dataset = this.state.dataset
+
+      await db.collection('questions').get().then(snapshots => {
+        snapshots.forEach(doc => {
+          const id = doc.id
+          const data = doc.data()
+          dataset[id] = data
+        })
+      })
+
+      this.initDataset(dataset)
+      const initAnswer = "";
+      this.selectAnswer(initAnswer, this.state.currentId)      
+    })()
   }
 
   componentDidUpdate(){ // スクロールの設定
---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB9APiNYAgEFauWyeuLIkx4dk12YXNCzUM",
+  authDomain: "chatapp-c0015.firebaseapp.com",
+  projectId: "chatapp-c0015",
+  storageBucket: "chatapp-c0015.appspot.com",
+  messagingSenderId: "615023110864",
+  appId: "1:615023110864:web:c1d3abe4a9b3127ce2ccd5",
+  measurementId: "G-MNN5KJZG7V"
+};
+
+export default firebaseConfig
